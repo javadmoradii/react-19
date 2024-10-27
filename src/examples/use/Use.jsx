414704a4487bad6f7c74bb481785a1bd76462ac0
moradi <moradi@mahsan.co>
2024-10-28 01:20:38 +0330

    refactor use

---
+++ src/examples/use/Use.jsx
@@ -0,0 +1,15 @@
+import Users from "./Users.jsx";
+import UserPrev from "./User_prev.jsx";
+
+const Use = () => <>
+    <h1>
+        USE
+    </h1>
+    <div className='flex columns-2 gap-10'>
+        <Users/>
+        <UserPrev/>
+    </div>
+</>
+
+
+export default Use
